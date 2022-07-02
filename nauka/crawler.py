3dc2136e37e5ae00bcc05705d1c954c450a745d8
Michelle-lele <fia_m@abv.bg>
2022-07-02 15:59:17 +0300

    Minor improvements to existing functionality - crawling categories and seed, other.

--- nauka/crawler.py
+++ nauka/crawler.py
@@ -1,15 +1,16 @@
+import sys
+
 import requests
 from nauka.scraper import Scraper
 
 
 class Crawler:
 
-    def __init__(self, base_url, data_path='/data'):
+    def __init__(self, base_url):
         self._base_url = base_url
-        self._data_path = data_path
         self._categories = []
         self._seed = []
-        self.publications = []
+        self._publications = []
 
     def run(self):
         """
@@ -21,6 +22,7 @@ class Crawler:
         """
         self.save_categories()
         self.get_seed()
+        self.save_publications()
 
     def save_categories(self):
         """
@@ -30,41 +32,55 @@ class Crawler:
         :return :list of tuples
 
         """
+        print("Crawling categories...")
         html = self.get_html(self._base_url)
         scraper = Scraper(html)
         self._categories = scraper.get_categories()
-        print(f"{len(self._categories)} categories extracted:")
-        print(*[category_name for category_url, category_name in self._categories], sep="\n")
+        print(f"{len(self._categories)} categories extracted!")
+        # print(*[category_name for category_url, category_name in self._categories], sep="\n")
         return self._categories
 
     def get_seed(self):
-        '''
+        """
         Finds all pages in the site categories and extracts the urls in a list.
 
         :param :None
-        :return :URLs :list
+        :return :string :list
 
-        '''
-        for path, category_name in self._categories:
-            current_page = 0
-            url = self._base_url + path + "?page_which=" + str(current_page)
-            html = self.get_html(url)
-            scraper = Scraper(html)
-            max_page = scraper.get_max_page()
-            while current_page < max_page:
+        """
+        if self._categories:
+            print("Crawling categories to get seed URLs...")
+            for path, category_name in self._categories:
+                current_page = 0
+                # TODO consider joining path under different OS
                 url = self._base_url + path + "?page_which=" + str(current_page)
-                self._seed.append(url)
-                current_page += 20
-            self._seed.append(self._base_url + path + "?page_which=" + str(max_page))
-            print(* self._seed, sep='\n')
+                html = self.get_html(url)
+                scraper = Scraper(html)
+                max_page = scraper.get_max_page()
+                while current_page < max_page:
+                    # TODO consider joining path under different OS
+                    url = self._base_url + path + "?page_which=" + str(current_page)
+                    self._seed.append(url)
+                    current_page += 20
+                # TODO consider joining path under different OS
+                self._seed.append(self._base_url + path + "?page_which=" + str(max_page))
+                # print(* self._seed, sep='\n')
+            print(f"{len(self._seed)} URLs extracted!")
+            return self._seed
+        else:
+            return sys.exit("No categories to crawl!")
 
     def save_publications(self):
         if self._seed:
             for url in self._seed:
                 html = self.get_html(url)
                 scraper = Scraper(html)
-                self.publications = scraper.get_publications(html)
-            # for publication in publications:
+                #to move it into the scraper?
+                all_publications = scraper.get_publications()
+            for publication in all_publications:
+                if scraper.is_pub_last_30_days(publication):
+                    self._publications.append(publication)
+            print(self._publications)
             #     # TODO if publication is in the past 30 days save the current category name, publication title, date and description
             #     pass
 
@@ -80,11 +96,11 @@ class Crawler:
         try:
             r = requests.get(url)
         except requests.RequestException:
-            # TODO handle
-            r = requests.get(url, verify=False)
-        except Exception as e:
-            print(f'Cannot get url: {url}: {str(e)}!')
-            exit(-1)
+            try:
+                r = requests.get(url, verify=False)
+            except Exception as e:
+                print(f'Cannot get url: {url}: {str(e)}!')
+                exit(-1)
 
         r.encoding = "utf-8"
 
--- nauka/scraper.py
+++ nauka/scraper.py
@@ -1,5 +1,8 @@
+import sys
+
 from bs4 import BeautifulSoup
 import re
+
 
 class Scraper:
     def __init__(self, html):
@@ -20,7 +23,7 @@ class Scraper:
             categories.append((link['href'], link.text))
         return categories
 
-    def get_publications(self, html):
+    def get_publications(self):
         """
             Extracts and returns the page publications.
 
@@ -29,10 +32,37 @@ class Scraper:
 
         """
         pubs_list = self._soup.find(class_="cat_list_s").findAll(class_=['cat_list_s_int', 'cat_list_box'])
-        print(len(pubs_list))
         return pubs_list
 
-    def is_pub_last_30_days(self):
+    def is_pub_last_30_days(self, publication):
+        """
+
+        :param publication:
+        :return:
+        """
+
+        """
+            <div class="cat_list_box">
+            <a href="/news/Razni_17/Uchenite-se-sheguvat_7102.html" title="Учените се шегуват">
+            <img src="//i2.offnews.bg/nauka/events/2015/03/28/7102/1427563124_2_152x158.jpg">
+            </img></a>
+            <div class="cat_list">
+            <h1 class="cat_list_title"><a href="/news/Razni_17/Uchenite-se-sheguvat_7102.html" title="Учените се шегуват">Учените се шегуват</a></h1>
+            <div class="news_info">01 април 2015 в 10:45<span class="eye_icon left_10px">17951</span><span class="comment_icon left_10px">2</span></div>
+            <div class="clb"></div><span class="cat_list_text">И учените обичат да се шегуват. Опитахме се да съберем малко доказателства за това твърдение. 
+            Какви са шегите на химици, физици, математици и биолози?
+            Химиците
+            
+            Таблицата на Менделеев отначало се е присънила на Пушкин, само че той нищо не е разбрал.
+            
+            "Пази се! Влажен...</span>
+            </div>
+            </div>
+
+        """
+        # r"\d\d \w[а-я,a-z]+ \d\d\d\d"gm
+        pub_date_div = self._soup.find(class_="news_info")
+        print(pub_date_div)
         pass
 
     def get_pub_date(self):
@@ -46,11 +76,13 @@ class Scraper:
 
     def get_max_page(self):
         div = self._soup.find(class_='pageBox')
-        href = div.findAll('a')[-1]['href']
-        rx = re.compile(r'\d+')
-        max_page = rx.search(href)
-        if max_page:
-            # print(max_page.group(0))
-            return int(max_page.group(0))
+        if div:
+            href = div.findAll('a')[-1]['href']
+            rx = re.compile(r'\d+')
+            max_page = rx.search(href)
+            if max_page:
+                return int(max_page.group(0))
+            else:
+                return 0
         else:
-            return 0
+            return sys.exit("Pages div not found!")
